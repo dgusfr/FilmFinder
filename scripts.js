51b3ee00cd90c3fdedf42713efefbdb6f26497b6
dgusfr <diegogustavo.fr@gmail.com>
2024-08-01 10:13:30 -0300

    dcripts

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Document is fully loaded");
 
-  // Função para manipulação do carrossel (placeholder)
   setupCarousel();
 
   setupSearch();
