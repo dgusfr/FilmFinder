8815fb9e5482c7284c9dfea6b0f5be74d60479ac
dgusfr <diegogustavo.fr@gmail.com>
2024-08-01 10:13:07 -0300

    function hover effects

--- scripts.js
+++ scripts.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // Função para manipulação da barra de pesquisa
   setupSearch();
 
-  // Função para manipulação de interações (hover effects, etc.)
   setupInteractions();
 });
 
