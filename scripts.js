38aeb98a859545523a75b9f8f648e302bfe941d1
dgusfr <diegogustavo.fr@gmail.com>
2024-08-04 07:33:00 -0300

    display serarch result

--- scripts.js
+++ scripts.js
@@ -96,3 +96,25 @@ function performSearch(query) {
       console.error("Error fetching search results:", error);
     });
 }
+
+function displaySearchResults(results) {
+  const searchResultsContainer = document.getElementById("search-results");
+  searchResultsContainer.innerHTML = "";
+
+  if (results) {
+    results.forEach((result) => {
+      const resultElement = document.createElement("div");
+      resultElement.className = "result";
+      resultElement.innerHTML = `
+              <img src="${result.Poster}" alt="${result.Title}">
+              <h3>${result.Title} (${result.Year})</h3>
+          `;
+      resultElement.addEventListener("click", () => {
+        loadMovieData(result.imdbID);
+      });
+      searchResultsContainer.appendChild(resultElement);
+    });
+  } else {
+    searchResultsContainer.innerHTML = "<p>No results found.</p>";
+  }
+}
