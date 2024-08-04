69724ea36e2259ff3a511a8599093979118cca72
dgusfr <diegogustavo.fr@gmail.com>
2024-08-04 07:30:14 -0300

    remove inner

--- scripts.js
+++ scripts.js
@@ -1,26 +1,4 @@
-function performSearch(query) {
-  const apiKey = "your_api_key_here"; // Substitua pela sua chave de API OMDb
-  const url = `https://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      displaySearchResults(data.Search);
-    })
-    .catch((error) => {
-      console.error("Error fetching search results:", error);
-    });
-}
-
-function displaySearchResults(results) {
-  const searchResultsContainer = document.getElementById("search-results");
-  searchResultsContainer.innerHTML = "";
-
-  if (results) {
-    results.forEach((result) => {
-      const resultElement = document.createElement("div");
-      resultElement.className = "result";
       resultElement.innerHTML = `
               <img src="${result.Poster}" alt="${result.Title}">
               <h3>${result.Title} (${result.Year})</h3>
