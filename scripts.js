d8d470686ba837f6a4ac0987ef61b1ecb5a8d759
dgusfr <diegogustavo.fr@gmail.com>
2024-08-06 08:17:08 -0300

    changes

--- scripts.js
+++ scripts.js
@@ -134,20 +134,6 @@ function displaySearchResults(results) {
     searchResultsContainer.innerHTML = "<p>No results found.</p>";
   }
 }
-function loadPopularMovies() {
-  const apiKey = "your_api_key_here"; // Substitua pela sua chave de API OMDb
-  const url = `https://www.omdbapi.com/?s=popular&type=movie&apikey=${apiKey}`;
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      displayCarousel(data.Search);
-    })
-    .catch((error) => {
-      console.error("Error fetching popular movies:", error);
-    });
-}
 
 function displayCarousel(movies) {
   const carousel = document.getElementById("carousel");
