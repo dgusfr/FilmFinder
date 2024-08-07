5c9626d0fbe8dfee962a15f1afb5848c565337d2
dgusfr <diegogustavo.fr@gmail.com>
2024-08-07 09:12:51 -0300

    refactor : scripts

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 // scripts.js
 
+let currentPage = 1;
+const itemsPerPage = 4; // Número de filmes favoritos por página
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Document is fully loaded");
 
@@ -21,24 +24,46 @@ document.addEventListener("DOMContentLoaded", () => {
   // Configurar favoritos
   setupFavorites();
 
+  // Configurar paginação
+  setupPagination();
+
   // Carregar e exibir favoritos
   displayFavorites();
 });
 
+function setupPagination() {
+  document.getElementById("prevPage").addEventListener("click", () => {
+    currentPage--;
+    displayFavorites();
+  });
+
+  document.getElementById("nextPage").addEventListener("click", () => {
+    currentPage++;
+    displayFavorites();
+  });
+}
+
 function displayFavorites() {
   const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
   const favoritesContainer = document.getElementById("favorites");
-  favoritesContainer.innerHTML = "";
+  const pageIndicator = document.getElementById("pageIndicator");
+  const prevPageBtn = document.getElementById("prevPage");
+  const nextPageBtn = document.getElementById("nextPage");
 
-  if (favorites.length > 0) {
-    favorites.forEach((movieId) => {
+  favoritesContainer.innerHTML = "";
+  const start = (currentPage - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const paginatedFavorites = favorites.slice(start, end);
+
+  if (paginatedFavorites.length > 0) {
+    paginatedFavorites.forEach((movieId) => {
       fetchMovieData(movieId, (movie) => {
         const movieElement = document.createElement("div");
         movieElement.className = "favorite-item";
         movieElement.innerHTML = `
-                  <img src="${movie.Poster}" alt="${movie.Title}">
-                  <h3>${movie.Title}</h3>
-              `;
+                    <img src="${movie.Poster}" alt="${movie.Title}">
+                    <h3>${movie.Title}</h3>
+                `;
         movieElement.addEventListener("click", () => {
           loadMovieData(movie.imdbID);
         });
@@ -48,6 +73,11 @@ function displayFavorites() {
   } else {
     favoritesContainer.innerHTML = "<p>No favorite movies found.</p>";
   }
+
+  // Atualizar estado dos botões de paginação
+  pageIndicator.textContent = `Página ${currentPage}`;
+  prevPageBtn.disabled = currentPage === 1;
+  nextPageBtn.disabled = end >= favorites.length;
 }
 
 function fetchMovieData(movieId, callback) {
