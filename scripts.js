cb65b7e04c66b496207650791e57a11707b0e475
dgusfr <diegogustavo.fr@gmail.com>
2024-08-07 09:13:07 -0300

    remove : carroussel

--- scripts.js
+++ scripts.js
@@ -6,7 +6,6 @@ const itemsPerPage = 4; // Número de filmes favoritos por página
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Document is fully loaded");
 
-  // Função para manipulação do carrossel
   setupCarousel();
 
   // Função para manipulação da barra de pesquisa
