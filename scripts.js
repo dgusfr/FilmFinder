1cb1c17e9110efe69524bbd53e7a0ba6728f16b8
dgusfr <diegogustavo.fr@gmail.com>
2024-08-09 08:03:37 -0300

    add show notification

--- scripts.js
+++ scripts.js
@@ -88,24 +88,8 @@ function showNotification(message) {
   notification.textContent = message;
   notification.classList.add("show");
 
+  // Remover a notificação após 3 segundos
   setTimeout(() => {
     notification.classList.remove("show");
   }, 3000);
-}
-
-function addFavorite(movieId) {
-  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  if (!favorites.includes(movieId)) {
-    favorites.push(movieId);
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-    showNotification("Filme adicionado aos favoritos!");
-  }
-}
-
-function removeFavorite(movieId) {
-  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  favorites = favorites.filter((id) => id !== movieId);
-  localStorage.setItem("favorites", JSON.stringify(favorites));
-  showNotification("Filme removido dos favoritos!");
-  displayFavorites();
 }
