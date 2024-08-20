2a26c7a899dd6376774432b5623acfc92127b719
dgusfr <diegogustavo.fr@gmail.com>
2024-08-20 19:41:45 -0300

    fix exemple id

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setupInteractions();
 
   // Carregar dados do filme
-  loadMovieData("tt0111161"); // Exemplo de ID de filme
+  loadMovieData("tt0111161");
 
   // Carregar filmes populares
   loadPopularMovies();
